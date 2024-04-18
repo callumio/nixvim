bb0a40c7dfe05ee8be6dd858f4892dc30fe82bb3
Callum Leslie <git@cleslie.uk>
2024-04-18 09:18:23 +0100

    more complete completion

--- config/plugins/cmp.nix
+++ config/plugins/cmp.nix
@@ -13,6 +13,8 @@
           "<CR>" = "cmp.mapping.confirm({ select = true })";
           "<C-Space>" = "cmp.mapping.complete({})";
         };
+        sources =
+          [ { name = "nvim_lsp"; } { name = "path"; } { name = "luasnip"; } ];
         snippet.expand = ''
           function(args)
             require('luasnip').lsp_expand(args.body)
